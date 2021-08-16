b0d95b12f7d507519c764ce14d00faa581eb2e2f
caramonica <caramonica@hotmail.co.uk>
2021-08-16 14:55:29 +0100

    fizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,3 +1,13 @@
 for (let i=0; i<=100; i++) {
-    console.log(i);
+    let message = '';
+    if (i % 3 === 0) {
+        message += 'Fizz';
+    }
+
+    if (i % 5 === 0) {
+        message += 'Buzz';
+    }
+
+    console.log(message === '' ? i : message);
+
 }
