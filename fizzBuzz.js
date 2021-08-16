9b084d3ef6a23588c13b0da67c70b97502e5da32
caramonica <caramonica@hotmail.co.uk>
2021-08-16 15:13:28 +0100

    added functionallity

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,36 @@
-for (let i=0; i<=100; i++) {
-    let message = '';
+const readline = require('readline-sync');
+
+const printWelcomeMessage = () => {
+    console.log('Welcome to FizzBuzz!')
+};
+
+const getNumber = (message) => {
+    console.log(`\n ${message}`);
+    return readline.prompt();
+}
+
+const fizzBuzz = (startN, endN) => {
+  for (let i = startN; i <= endN; i++) {
+    let message = "";
+
     if (i % 3 === 0) {
-        message += 'Fizz';
+      message += "Fizz";
     }
 
     if (i % 5 === 0) {
-        message += 'Buzz';
+      message += "Buzz";
     }
 
-    console.log(message === '' ? i : message);
+    console.log(message === "" ? i : message);
+  };
+}
 
+const runFizzBuzz = () => {
+    fizzBuzz(getNumber('\nStart number:'), getNumber('\nEnd number:'))
+}
+
+
+printWelcomeMessage();
+while (true) {
+    runFizzBuzz();
 }
